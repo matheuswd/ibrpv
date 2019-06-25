ff406170db87b17e7c9c0b8bcaa25da856388d15
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-06-25 23:53:49 +0200

    adds the service section

---
+++ template-parts/flexible-content/services.php
@@ -0,0 +1,24 @@
+<?php
+
+$args = wp_parse_args($args, [
+    'services' => ($args['services']) ? $args['services'] : false,
+]);
+
+$services = $args['the_services'];
+$services_columns = sizeof($services);
+
+?>
+
+<section class="service-time">
+    <?php
+    foreach ($services as $service) {
+        ?>
+        <div class="service-card">
+            <h2 class="service-card__time"><?php echo esc_attr($service['service_time']) ?></h2>
+            <span class="service_card__day"><?php echo esc_attr($service['service_day']) ?></span> | 
+            <span class="service_card__type"><?php echo esc_attr($service['service_type']) ?></span>
+        </div>
+        <?php
+    }
+    ?>
+</section>
