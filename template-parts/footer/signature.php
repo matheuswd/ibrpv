622ca101499fbf39a8e96aa8b2866ead76e4c3b3
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-07-02 01:11:59 +0200

    creates the html markup and the php data as well

--- template-parts/footer/signature.php
+++ template-parts/footer/signature.php
@@ -1,4 +1,32 @@
-<?php esc_html_e('Website by', 'granola'); ?>
-<a href="https://www.wholegraindigital.com/" rel="designer">
-<?php esc_html_e('Wholegrain Digital', 'granola'); ?>
-</a>
+<?php
+
+$fb_icon = get_field('fb_icon', 'option');
+$youtube_icon = get_field('youtube_icon', 'option');
+$instagram_icon = get_field('instagram_icon', 'option');
+$footer_message = get_field('footer_message', 'option');
+
+?>
+
+<section class="icons">
+    <?php if ($fb_icon) : ?>
+        <div class="icons__fb">
+            <img src="<?php echo $fb_icon['url']; ?>" alt="<?php echo $fb_icon['alt'] ?>">
+        </div>
+    <?php endif; ?>
+    <?php if ($youtube_icon) : ?>
+        <div class="icons__youtube">
+            <img src="<?php echo $youtube_icon['url']; ?>" alt="<?php echo $youtube_icon['alt'] ?>">
+        </div>
+    <?php endif; ?>
+    <?php if ($instagram_icon) : ?>
+        <div class="icons__instagram">
+            <img src="<?php echo $instagram_icon['url']; ?>" alt="<?php echo $instagram_icon['alt'] ?>">
+        </div>
+    <?php endif; ?>
+</section>
+
+<?php if ($footer_message) : ?>
+    <section class="message">
+        <p><?php echo esc_attr($footer_message); ?></p>
+    </section>
+<?php endif;
