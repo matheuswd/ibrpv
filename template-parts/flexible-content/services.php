76eea0138a30ee17352bca857eb3268fc3d09c43
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-07-18 12:40:55 -0300

    creates new markup for visiting section

--- template-parts/flexible-content/services.php
+++ template-parts/flexible-content/services.php
@@ -12,31 +12,22 @@ $link_google_maps = $args['link_google_maps'];
 
 ?>
 
-<div class="services-section">
-    <section class="container service-time">
-            <div class="service-card">
-                <div class="service-card__line">
-                    <img src="<?php echo get_template_directory_uri(). '/assets/images/calendar.png'; ?>" alt="Imagem de calendário para representar a agenda da Igreja Batista Reformada Palavra Viva" class="icon">
-                    <p><?php echo esc_html($title_1); ?></p>
-                </div>
-                <div class="service-card__line">
-                    <p class="line__description"><?php echo esc_html($description); ?></p>
-                </div>
+<section class="church-info">
+    <div class="container">
+        <div class="column visiting">
+            <p class="visiting__title"><img src="<?php echo get_template_directory_uri() . '/assets/images/calendar.png' ?>" class="icon" id="calendar" alt="">Venha nos visitar</p>
+            <p class="visiting__time">Terça-feira às 20h - Domingo às 19h</p>
+            <hr>
+        </div>
+        <div class="column address">
+            <div class="address__info">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/maps-and-flags.png' ?>" class="icon" id="maps-and-flags" alt="">
+                <p class="address__title"><img src="<?php echo get_template_directory_uri() . '/assets/images/maps-and-flags.png' ?>" class="icon" id="maps-and-flags-large" alt="">Igreja Batista Reformada Palavra Viva</p>
+                <p class="address__street-address"><img src="<?php echo get_template_directory_uri() . '/assets/images/location-arrow.png' ?>" class="icon" id="location-arrow" alt="">Avenida Flavio Fulvio Biazzi, 280</p>
             </div>
-            <div class="service-card">
-                <div class="service-card__line">
-                    <img src="<?php echo get_template_directory_uri(). '/assets/images/maps-and-flags.png'; ?>" alt="Imagem de localização para representar a localização da Igreja Batista Reformada Palavra Viva" class="icon">
-                    <p class="line__church-name"><?php echo esc_html($church_name); ?></p>
-                </div>
-                <div class="service-card__line">
-                    <img src="<?php echo get_template_directory_uri(). '/assets/images/location-arrow.png'; ?>" alt="Ícone de mapa para representar a localização da Igreja Batista Reformada Palavra Viva" class="icon">
-                    <p class="line__church-address"><?php echo esc_html($address); ?></p>
-                </div>
+            <div class="address__gmaps">
+                <button class="button--white-dark-blue"><a href="#">Google Maps</a></button>
             </div>
-            <div class="service-card">
-                <div class="service-card__line">
-                    <button class="button--white-dark-blue"><a href="<?php echo esc_html($link_google_maps) ?>"><?php esc_html_e('Google Maps', 'ibrpv'); ?></a></button>
-                </div>
-            </div>
-    </section>
-</div>
+        </div>
+    </div>
+</section>
