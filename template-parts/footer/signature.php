d472e18e3b3b4b7d8b8ec458faa3f0530c61ee89
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-07-19 15:38:46 -0300

    new footer html

--- template-parts/footer/signature.php
+++ template-parts/footer/signature.php
@@ -13,10 +13,23 @@ $instagram_icon_link = get_field('instagram_icon_link
 ', 'option');
 
 //Message
+$logo = get_field('no_text_logo', 'option');
 $footer_message = get_field('footer_message', 'option');
+$learn_more = get_field('learn_more', 'option');
+$learn_more_url = get_field('learn_more_url', 'option');
 
 ?>
 
+<?php if ($footer_message) : ?>
+    <section class="message">
+        <img src="<?php echo esc_html($logo); ?>" alt="">
+        <p class="footer-message"><?php echo esc_attr($footer_message); ?></p>
+        <p><a href="<?php echo esc_attr($learn_more_url); ?>"><?php echo esc_attr($learn_more); ?></a></p>
+    </section>
+<?php endif; ?>
+<div>
+    <span class="follow-us"><?php esc_html_e('Siga-nos', 'IBRPV'); ?></span>
+</div>
 <section class="icons">
     <?php if ($fb_icon && $fb_icon_link ) : ?>
         <div class="icons__fb">
@@ -40,9 +53,3 @@ $footer_message = get_field('footer_message', 'option');
         </div>
     <?php endif; ?>
 </section>
-
-<?php if ($footer_message) : ?>
-    <section class="message">
-        <p><?php echo esc_attr($footer_message); ?></p>
-    </section>
-<?php endif;
