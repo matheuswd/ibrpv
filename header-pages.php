2cecc97f63f8df06926cdb66ea1e7674a4dab235
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-07-26 22:38:47 -0300

    adds full image on header for all pages

---
+++ header-pages.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php echo granola_render('template-parts/header/skip-link'); ?>
+    <div class="header-wrap Matheus">
+    <header class="header">
+        <div class="header__inner container">
+            <a class="header__logo" href="<?php echo esc_url(home_url('/')); ?>">
+            <?php echo granola_svg(get_field('light_logo', 'option')['name'], [
+                'title' => get_bloginfo('name'),
+                'description' => get_bloginfo('description'),
+            ]); ?>
+            <?php echo granola_svg(get_field('dark_logo', 'option')['name'], [
+                'title' => get_bloginfo('name'),
+                'description' => get_bloginfo('description'),
+            ]); ?>
+            </a>
+            <?php echo granola_render('template-parts/header/menu'); ?>
+        </div>
+    </header>
+    </div>
+    <div class="site" id="maincontent">
