00c2bf8c20e5c62ecf34116f99d69d9b7c575b67
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-07-31 23:00:46 -0300

    archive file for the categories

---
+++ archive.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<?php
+if(!empty(get_option('page_for_posts'))) {
+    $img = wp_get_attachment_image_src(get_post_thumbnail_id(get_option('page_for_posts')), 'full');
+    $featured_image = $img[0];
+}
+?>
+
+<section class="hero" style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('<?php echo $featured_image; ?>')">
+    <div class="container hero-inner">
+        <div class="intro-text">
+            <h1><?php wp_title(''); ?></h1>
+        </div>
+    </div>
+</section>
+
+<?php if (have_posts()) { ?>
+<div class="container">
+<?php if(is_blog()) {
+            echo granola_render('template-parts/post/categories');
+        }
+        ?>
+</div>
+<div class="container spacing">
+    <main>
+    <?php echo granola_render('template-parts/flexible-content/all-posts', array('show_posts' => true, 'number_of_posts' => 9, 'categories' => array(get_queried_object()->term_id) )); ?>
+    </main>
+    <?php echo granola_render('template-parts/wordpress/posts-pagination'); ?>
+</div>
+    
+<?php } else { ?>
+    <main class="container">
+        <?php echo granola_render('template-parts/content-none'); ?>
+    </main>
+<?php } ?>
+
+<?php get_footer();?>
