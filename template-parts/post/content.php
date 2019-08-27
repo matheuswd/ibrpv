5311f7dfe151d1e171d7953bb52326001f88cfc2
Matheus Martins <matheusdealmeidamartins@gmail.com>
2019-08-27 12:05:32 -0300

    adds the date and sharing section to the website

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -9,32 +9,28 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php
-        if (is_single()) :
-        ?>
-        <section class="post-content">
-            <span class="post-info">
-                <?php the_date('d/m/Y'); ?> | 
-                <span><?php the_category(' • '); ?></span>
-            </span>
-            <?php the_content(); ?>
-        </section>
-        <?php get_sidebar(); ?>
-        <section class="sharing">
-            <span><?php esc_html_e('Compartilhar', 'ibrpv') ?></span>
-            <div class="icons">
-                <div class="icons__fb">
-                    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank">
-                        <?php echo granola_image('social-icons/icon-fb-grey.png', array(
-                            'alt' => __('Ícone de compartilhamento do Facebook', 'ibrpv')
-                        )) ?>
-                    </a>
+        <?php if (is_single()) : ?>
+            <section class="post-content">
+                <span class="post-info">
+                    <?php the_date('d/m/Y'); ?> | 
+                    <span><?php the_category(' • '); ?></span>
+                </span>
+                <?php the_content(); ?>
+            </section>
+            <?php get_sidebar(); ?>
+            <section class="sharing">
+                <span><?php esc_html_e('Compartilhar', 'ibrpv') ?></span>
+                <div class="icons">
+                    <div class="icons__fb">
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink(); ?>" target="_blank">
+                            <?php echo granola_image('social-icons/icon-fb-grey.png', array(
+                                'alt' => __('Ícone de compartilhamento do Facebook', 'ibrpv')
+                            )) ?>
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <?php
-        endif;
-        ?>
+            </section>
+        <?php endif; ?>
 
     <footer class="entry-footer">
 
