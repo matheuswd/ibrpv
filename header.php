c7f238a6b605943d77e487123590d7f73581990f
Matheus Martins <matheusdealmeidamartins@gmail.com>
2020-02-04 18:36:50 -0300

    Adding Pinterest Meta tag

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="p:domain_verify" content="18b7239c70cc80a2b2110e3aa51fc8d2"/>
     <link rel="icon" type="image/png" href="<?php the_field('favicon', 'options'); ?>">
     <?php wp_head(); ?>
 </head>
